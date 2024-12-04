b629af552044cfdb65aac0d5194a7f4293807050
vincent DONVAL <vincent.donval@avisto.com>
2024-12-04 16:16:51 +0100

    adapt main.py

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
-from chargingstationmergedtool.parser.OsmParser import OsmParser
-from chargingstationmergedtool.parser.DataGouvParser import DataGouvParser
-from chargingstationmergedtool.Transform import Transform
-from chargingstationmergedtool.Config import Config
+from chargingstationmergedtool.chargingstationmergetool import ChargingStationMergeTools
 from argparse import ArgumentParser
-import os
 
-parser = ArgumentParser(prog="", description="A tool for merged French charging stations datasources")
-parser.add_argument('-c', '--config_file', required=True)
+if __name__ == '__main__':
+    parser = ArgumentParser(prog="", description="A tool for merged French charging stations datasources")
+    parser.add_argument('-c', '--config_file', required=True)
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-config = Config(args.config_file)
-
-
-
-
-
-
+    chargingStationMergeTools = ChargingStationMergeTools(args.config_file)
+    chargingStationMergeTools.process()
